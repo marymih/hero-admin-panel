d65c189a216d4a2f9ef02bc411afe28d48d4bdce
Mariia Mikhalchenko <m.mikhalchenko.de@gmail.com>
2024-11-04 11:44:20 +0100

    add deleteItem function

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -1,5 +1,5 @@
 import {useHttp} from '../../hooks/http.hook';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { heroesFetching, heroesFetched, heroesFetchingError } from '../../actions';
@@ -12,18 +12,27 @@ import Spinner from '../spinner/Spinner';
 // Удаление идет и с json файла при помощи метода DELETE
 
 const HeroesList = () => {
-    const {heroes, heroesLoadingStatus} = useSelector(state => state);
+    const [heroes, setHeroes] = useState([]);
+    const {heroesLoadingStatus} = useSelector(state => state.heroesLoadingStatus);
     const dispatch = useDispatch();
     const {request} = useHttp();
 
     useEffect(() => {
         dispatch(heroesFetching());
         request("http://localhost:3001/heroes")
-            .then(data => dispatch(heroesFetched(data)))
+            .then(data => {
+                setHeroes(data);
+                dispatch(heroesFetched(data));
+            })
             .catch(() => dispatch(heroesFetchingError()))
 
         // eslint-disable-next-line
-    }, []);
+    }, [dispatch, request]);
+
+    const deleteItem = (id) => {
+        const newHeroes = heroes.filter(item => item.id !== id);
+        setHeroes(newHeroes);
+    }
 
     if (heroesLoadingStatus === "loading") {
         return <Spinner/>;
@@ -37,7 +46,7 @@ const HeroesList = () => {
         }
 
         return arr.map(({id, ...props}) => {
-            return <HeroesListItem key={id} {...props}/>
+            return <HeroesListItem key={id} {...props} onDelete={() => deleteItem(id)}/>
         })
     }
 
