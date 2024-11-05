3964f3966c48ad4495214823d7e8e22719e44f55
Mariia Mikhalchenko <m.mikhalchenko.de@gmail.com>
2024-11-05 21:21:35 +0100

    combineReducers, createSelector

--- src/components/heroesAddForm/HeroesAddForm.js
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -10,7 +10,9 @@ const HeroesAddForm = () => {
   const [description, setDescription] = useState('');
   const [element, setElement] = useState('');
 
-  const { filters, filtersLoadingStatus } = useSelector((state) => state);
+  const { filters, filtersLoadingStatus } = useSelector(
+    (state) => state.filters
+  );
   const dispatch = useDispatch();
   const { request } = useHttp();
 
