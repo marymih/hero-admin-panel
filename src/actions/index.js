027ea5a1c9991c455556a4559d76665013ae198f
Mariia Mikhalchenko <m.mikhalchenko.de@gmail.com>
2024-11-07 20:12:25 +0100

    RTK: createAction, createReducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import { createAction } from '@reduxjs/toolkit';
+
 export const fetchHeroes = (request) => (dispatch) => {
   dispatch(heroesFetching());
   request('http://localhost:3001/heroes')
@@ -12,38 +14,48 @@ export const fetchFilters = (request) => (dispatch) => {
     .catch(() => dispatch(filtersFetchingError()));
 };
 
-export const heroesFetching = () => {
-  return {
-    type: 'HEROES_FETCHING',
-  };
-};
+// export const heroesFetching = () => {
+//   return {
+//     type: 'HEROES_FETCHING',
+//   };
+// };
 
-export const heroesFetched = (heroes) => {
-  return {
-    type: 'HEROES_FETCHED',
-    payload: heroes,
-  };
-};
+export const heroesFetching = createAction('HEROES_FETCHING');
 
-export const heroesFetchingError = () => {
-  return {
-    type: 'HEROES_FETCHING_ERROR',
-  };
-};
+// export const heroesFetched = (heroes) => {
+//   return {
+//     type: 'HEROES_FETCHED',
+//     payload: heroes,
+//   };
+// };
 
-export const heroAdded = (hero) => {
-  return {
-    type: 'HERO_ADDED',
-    payload: hero,
-  };
-};
+export const heroesFetched = createAction('HEROES_FETCHED');
 
-export const heroDeleted = (id) => {
-  return {
-    type: 'HERO_DELETED',
-    payload: id,
-  };
-};
+// export const heroesFetchingError = () => {
+//   return {
+//     type: 'HEROES_FETCHING_ERROR',
+//   };
+// };
+
+export const heroesFetchingError = createAction('HEROES_FETCHING_ERROR');
+
+// export const heroAdded = (hero) => {
+//   return {
+//     type: 'HERO_ADDED',
+//     payload: hero,
+//   };
+// };
+
+export const heroAdded = createAction('HERO_ADDED');
+
+// export const heroDeleted = (id) => {
+//   return {
+//     type: 'HERO_DELETED',
+//     payload: id,
+//   };
+// };
+
+export const heroDeleted = createAction('HERO_DELETED');
 
 export const filtersFetching = () => {
   return {
